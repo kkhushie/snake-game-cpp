94f5cadac7590e5755b3c7d48f028ec93375f163
Khushie <111669586+kkhushie@users.noreply.github.com>
2025-09-26 11:25:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using std namespace;
+
+const int width = 20;
+const int height = 10;
+
 int main(){
+
+   // Draw top border
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
 return 0;
 }
